4eaf447ab9051d0dc6b17176d3885f100b7eb567
Rasmus Andersson <rasmus@notion.se>
2020-05-23 13:43:05 -0700

    test/watchdir/test.sh

--- test/watchdir/test.sh
+++ test/watchdir/test.sh
@@ -6,10 +6,11 @@ mkdir -p node_modules
 ln -s ../../..                       node_modules/estrella
 ln -s ../../../node_modules/esbuild  node_modules/esbuild
 
-export ESTRELLA_TEST_VERBOSE=0
+export ESTRELLA_TEST_VERBOSE=$ESTRELLA_TEST_VERBOSE
 if [ "$1" == "-verbose" ]; then
   export ESTRELLA_TEST_VERBOSE=1
 fi
 
-echo test-cli.js ; node test-cli.js
-echo test-api.js ; node test-api.js
+for f in test-*.js; do
+  echo "$f" ; node --unhandled-rejections=strict "$f"
+done
