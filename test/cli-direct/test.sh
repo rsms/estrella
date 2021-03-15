f5c96ee27410404b8e4239c389d54f262ed4b0f7
Rasmus Andersson <rasmus@notion.se>
2021-03-15 12:37:01 -0700

    fixup test/cli-direct for more recent esbuild behavior

--- test/cli-direct/test.sh
+++ test/cli-direct/test.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -e
+#!/bin/bash
+set -e
 cd "$(dirname "$0")"
 
 estrella=$PWD/node_modules/estrella/dist/estrella.js
@@ -12,16 +13,16 @@ mkdir -p node_modules
 ln -s ../../..                       node_modules/estrella
 ln -s ../../../node_modules/esbuild  node_modules/esbuild
 
-function fail {
+_fail() {
   echo "FAIL $1" >&2
   exit 1
 }
 
-function test {
+_test() {
   echo "run test $1"
 }
 
-function _clean {
+_clean() {
   rm -f "$outfile"
 }
 trap _clean EXIT
@@ -30,78 +31,84 @@ trap exit SIGINT  # make sure we can ctrl-c in loops
 _clean
 
 
-test "should write to outfile"
+_test "should write to outfile"
 "$estrella" "-outfile=$outfile" main.ts -quiet
-expect='console.log("Hello world");'
+expect='console.log("Hello world")'
 actual=$(cat "$outfile")
 if [[ "$("$estrella" main.ts)" != "$expect"* ]]; then
-  fail "Unexpected output to $outfile. Expected [${expect}*] but got [$actual]"
+  _fail "Unexpected output to $outfile. Expected [${expect}*] but got [$actual]"
 fi
+echo "PASS"
 
-test "should run outfile when -run is set"
+_test "should run outfile when -run is set"
 expect='Hello world'
 actual=$("$estrella" "-outfile=$outfile" main.ts -quiet -run)
 if [[ "$actual" != "$expect" ]]; then
-  fail "Unexpected output (-run). Expected [$expect] but got [$actual]"
+  _fail "Unexpected output (-run). Expected [$expect] but got [$actual]"
 fi
+echo "PASS"
 
-test "Should print to stdout when no outfile is given."
+_test "Should print to stdout when no outfile is given."
 # Also there should be no "Wrote" log message.
-expect='console.log("Hello world");'
+expect='console.log("Hello world")'
 actual=$("$estrella" main.ts)
 if [[ "$actual" != "$expect"* ]]; then
-  fail "Unexpected output to stdout. Expected [${expect}*] but got [$actual]"
+  _fail "Unexpected output to stdout. Expected [${expect}*] but got [$actual]"
 fi
+echo "PASS"
 
-test "Should run rather than print to stdout when no outfile is given and -run is set"
+_test "Should run rather than print to stdout when no outfile is given and -run is set"
 # Also there should be no "Wrote" log message.
 expect='Hello world'
 actual=$("$estrella" main.ts -run)
 if [[ "$actual" != "$expect" ]]; then
-  fail "Unexpected stdout (-run). Expected [$expect] but got [$actual]"
+  _fail "Unexpected stdout (-run). Expected [$expect] but got [$actual]"
 fi
+echo "PASS"
 
 # ------------------------
 # infile inference
 
-test "should fail to infer input files (no tsconfig.json)"
+_test "should fail to infer input files (no tsconfig.json)"
 set +e
 actual=$("$estrella" 2>&1)
 set -e
 if [[ "$actual" != *"missing <srcfile>"* ]]; then
-  fail "Should have failed with no inputs, with '... missing <srcfile> ...'. Got $actual"
+  _fail "Should have failed with no inputs, with '... missing <srcfile> ...'. Got $actual"
 fi
+echo "PASS"
 
 
 pushd ts-files >/dev/null
-test "(ts-files) infile inference from tsconfig.json:files; -run"
+_test "(ts-files) infile inference from tsconfig.json:files; -run"
 expect='Hello world'
 actual=$("$estrella" "-outfile=../$outfile" -quiet -run -no-diag)
 if [[ "$actual" != "$expect" ]]; then
-  fail "Unexpected output (-run). Expected [$expect] but got [$actual]"
+  _fail "Unexpected output (-run). Expected [$expect] but got [$actual]"
 fi
+echo "PASS"
 popd >/dev/null
 
 
 pushd ts-include >/dev/null
-test "(ts-include) infile inference from tsconfig.json:include (glob); -run"
+_test "(ts-include) infile inference from tsconfig.json:include (glob); -run"
 expect='Hello world'
 actual=$("$estrella" "-outfile=../$outfile" -quiet -run -no-diag)
 if [[ "$actual" != "$expect" ]]; then
-  fail "Unexpected output (-run). Expected [$expect] but got [$actual]"
+  _fail "Unexpected output (-run). Expected [$expect] but got [$actual]"
 fi
+echo "PASS"
 popd >/dev/null
 
 
 pushd ts-diag >/dev/null
 
-test "(ts-diag) infile inference from tsconfig.json:files; -diag should report an error"
+_test "(ts-diag) infile inference from tsconfig.json:files; -diag should report an error"
 expect="TS2304: Cannot find name 'not_exist'"
 actual=$("$estrella" -diag 2>&1)
 if [[ "$actual" != *"$expect"* ]]; then
-  fail "Unexpected output (-run). Expected [*${expect}*] but got [$actual]"
+  _fail "Unexpected output (-run). Expected [*${expect}*] but got [$actual]"
 fi
+echo "PASS"
 popd >/dev/null
 
-
-echo "OK"
